16870be88469bbba9ed701a29ddd182884b63488
sanskarpareek14 <pareek081@gmail.com>
2021-11-04 14:49:59 +0530

    create react app

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem Ipsum is simply dummy text
+      <h4>I m h4</h4>
+      <h4>I m h4</h4>
+      <h4>I m h4</h4>
+      <h4>I m h4</h4>
+      <h4>I m h4</h4>
+    </div>
+  );
+}
+
+export default App;
