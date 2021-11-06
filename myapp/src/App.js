28dad5b41dcc3ff60e82698193a4bd74732d0219
sanskarpareek14 <pareek081@gmail.com>
2021-11-06 17:27:49 +0530

    same

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,9 +4,6 @@ import Content from './Components/Content'
 
 const App = () => {
 
-  const chores = ["item 1", "item 2", "item 3", "item 4"]
-
-
   return (
     <div className="App">
       <Navbar />
