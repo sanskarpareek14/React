f803ad6af8c8b202da6f5df75f89100e75aa2b4b
sanskarpareek14 <pareek081@gmail.com>
2021-11-08 00:23:20 +0530

    useState

--- myapp/src/Components/Content.js
+++ myapp/src/Components/Content.js
@@ -1,17 +1,43 @@
 import ListItem from './ListItem'
-
+import { useState } from 'react'
 const Content = () => {
-    const chores = ["item 1", "item 2", "item 3", "item 4"]
+    // let counter = 0
+    let [counter, setCounter] = useState(0)
+    // console.log(counter)
+    // console.log(mysteryfunction)
+    console.log('rerender')
     return (
-        <div class="container">
-            <ul>
-                {
-                    chores.map(chore => <ListItem chore={chore} />)
-                }
-
-            </ul>
+        <div className="container">
+            <h2>{counter}</h2>
+            <button onClick={() => {
+                console.log(counter)
+                counter = counter + 1
+                setCounter(previousValue => {
+                    console.log(previousValue)
+                    return counter
+                })
+            }}>Click Me</button>
         </div>
     );
 }
 
 export default Content;
+
+// import React from "react";
+
+// class Content extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             count: 0
+//         };
+//     }
+//     render() {
+//         return (
+//             <div className="container">
+//                 Lorem ipsum dolor sit amet consecture adispsj elit.COnseehguer aliquid debits expeditia.
+//             </div>
+//         )
+//     }
+// }
+// export default Content;
