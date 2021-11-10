b4bfe952d3ee6750581ea769b0713e83f3940ae6
sanskarpareek14 <pareek081@gmail.com>
2021-11-10 22:12:30 +0530

    React add

--- myapp/src/Components/Tap.js
+++ myapp/src/Components/Tap.js
@@ -1,14 +1,17 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Tap = () => {
     useEffect(() => {
         const alert = () => window.alert('Hey!! You just double clicked')
         document.addEventListener('dblclick', alert)
+        return () => document.removeEventListener('dblclick', alert)
     }, [])
+    const [count, setCount] = useState(0)
     return (
         <div className="container">
             <div className="tap">
-                <h2>Tap</h2>
+                <h2>{count}</h2>
+                <button onClick={() => setCount(prev => prev + 1)}>Click</button>
             </div>
         </div>
     );
--- myapp/src/Components/Timer.js
+++ myapp/src/Components/Timer.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 const Timer = () => {
     return (
         <div className="container">
